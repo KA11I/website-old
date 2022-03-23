a5bc26143667d389762170a953d9d71290a04623
KA11I <75325289+KA11I@users.noreply.github.com>
2022-03-23 12:21:04 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-lol
+var i = 0;
+var txt = "Hey, I'm Karl...";
+var speed = 100;
+function typewriter() {
+    if (i < txt.length) {
+        document.getElementById("title").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typewriter, speed);
+    }
+}
