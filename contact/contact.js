a69a9e9bac033943347a5f7946025900964a0436
KA11I <75325289+KA11I@users.noreply.github.com>
2022-04-09 02:39:55 +0200

    Update contact.js

--- contact/contact.js
+++ contact/contact.js
@@ -1,1 +1,10 @@
-lol
+var i = 0;
+var txt = "Send me a message!";
+var speed = 100;
+function typewriter() {
+    if (i < txt.length) {
+        document.getElementById("title").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typewriter, speed);
+    }
+}
