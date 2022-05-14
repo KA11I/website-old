25cb96c9634f7d4b7ecd72342ef86ec988c41b77
KA11I <75325289+KA11I@users.noreply.github.com>
2022-05-14 22:16:54 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -11,23 +11,16 @@ function typewriter() {
 }
 
 async function changeImage() {
-    if (document.querySelector('#imgClickAndChange')) {
-        const images = [
-            'crepe/Crepe3.png',
-            'crepe/Crepe2.png',
-            'crepe/Crepe1.png',
-            'crepe/Crepe0.png',
-        ];
-        if (
-            document.getElementById('imgClickAndChange').src ==
-            'crepe/Crepe4.png'
-        ) {
-            for (let img of images) {
-                document.querySelector('#imgClickAndChange').src = img;
-                await new Promise((resolve) => setTimeout(resolve, 1000));
-            }
-        }
-    }
+  const images = [
+    'crepe/Crepe3.png',
+    'crepe/Crepe2.png',
+    'crepe/Crepe1.png',
+    'crepe/Crepe0.png',
+  ];
+  for (let img of images) {
+    document.querySelector('#imgClickAndChange').src = img;
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  }
 }
 
 document.querySelector('#imgClickAndChange').onclick = changeImage
