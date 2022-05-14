09713e91a522f8268806178608fb5be9993781af
KA11I <75325289+KA11I@users.noreply.github.com>
2022-05-14 20:34:22 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -11,9 +11,9 @@ function typewriter() {
 }
 
 function changeImage() {
-    if (document.getElementById("imgClickAndChange").src == "http://www.userinterfaceicons.com/80x80/minimize.png"){
-        document.getElementById("imgClickAndChange").src = "http://www.userinterfaceicons.com/80x80/maximize.png";
+    if (document.getElementById("imgClickAndChange").src == "https://www.ka11i.com/ka11i.png"){
+        document.getElementById("imgClickAndChange").src = "https://cdn.discordapp.com/attachments/677498889900785695/975103555889410208/dump_face.jpg";
     } else {
-        document.getElementById("imgClickAndChange").src = "http://www.userinterfaceicons.com/80x80/minimize.png";
+        document.getElementById("imgClickAndChange").src = "https://cdn.discordapp.com/attachments/677498889900785695/975103555889410208/dump_face.jpg";
     }
 }
