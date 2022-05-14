cadab95788061abefca104e8103bfbfc088bf641
KA11I <75325289+KA11I@users.noreply.github.com>
2022-05-14 21:33:40 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -13,23 +13,23 @@ function typewriter() {
 function changeImage() {
   if (
     document.getElementById('imgClickAndChange').src ==
-    'https://www.ka11i.com/ka11i.png'
+    '/crepe/Crepe4.png'
   ) {
     const imageWithTimeout = [
       {
-        src: 'https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fwww.uni-kassel.de%2Funi%2Findex.php%3FeID%3DdumpFile%26t%3Dp%26p%3D63987%26token%3D7a38fdf79a187b68173b4466540fe6b936294e61&sp=1652554349Tab0038b84085c7710f3f44eb987349eee4e2395e072980b7e44b5d7a48a50cad',
+        src: '/crepe/Crepe3.png',
         timeout: 1000,
       },
       {
-        src: 'https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fimage.geo.de%2F30065630%2Ft%2Fk4%2Fv4%2Fw1440%2Fr0%2F-%2Ffliegenpilz-gross-01-jpg--30419-.jpg&sp=1652554349T911167e3661a32720c8af807b41cc54573df3a6fe07dc4575a37f53fd1f5dfe9',
+        src: '/crepe/Crepe2.png',
         timeout: 2000,
       },
       {
-        src: 'https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fwww.uni-kassel.de%2Funi%2Findex.php%3FeID%3DdumpFile%26t%3Dp%26p%3D63987%26token%3D7a38fdf79a187b68173b4466540fe6b936294e61&sp=1652554349Tab0038b84085c7710f3f44eb987349eee4e2395e072980b7e44b5d7a48a50cad',
+        src: '/crepe/Crepe1.png',
         timeout: 3000,
       },
       {
-        src: 'https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fimage.geo.de%2F30065630%2Ft%2Fk4%2Fv4%2Fw1440%2Fr0%2F-%2Ffliegenpilz-gross-01-jpg--30419-.jpg&sp=1652554349T911167e3661a32720c8af807b41cc54573df3a6fe07dc4575a37f53fd1f5dfe9',
+        src: '/crepe/Crepe0.png',
         timeout: 4000,
       },
     ];
