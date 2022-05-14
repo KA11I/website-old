a828c8d1345f3c12494dc0c33e0452c45fddf166
KA11I <75325289+KA11I@users.noreply.github.com>
2022-05-14 22:23:21 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,24 +3,29 @@ var txt = "Hey, I'm Karl...";
 var speed = 100;
 
 function typewriter() {
-    if (i < txt.length) {
-        document.getElementById("title").innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typewriter, speed);
-    }
-}
-
-async function changeImage() {
-  const images = [
-    'crepe/Crepe3.png',
-    'crepe/Crepe2.png',
-    'crepe/Crepe1.png',
-    'crepe/Crepe0.png',
-  ];
-  for (let img of images) {
-    document.querySelector('#imgClickAndChange').src = img;
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+  if (i < txt.length) {
+    document.getElementById('title').innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typewriter, speed);
   }
 }
-document.querySelector('#imgClickAndChange').onclick = changeImage
-changeImage()
+
+let changeImageBool = true;
+
+async function changeImage() {
+  if (changeImageBool) {
+    const images = [
+      'crepe/Crepe3.png',
+      'crepe/Crepe2.png',
+      'crepe/Crepe1.png',
+      'crepe/Crepe0.png',
+    ];
+    for (let img of images) {
+      document.querySelector('#imgClickAndChange').src = img;
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+    changeImageBool = false;
+  }
+}
+
+document.querySelector('#imgClickAndChange').onclick = changeImage;
