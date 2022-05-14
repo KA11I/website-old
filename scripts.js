14c6dae0927a21b05c537437628c3c8f237700c2
KA11I <75325289+KA11I@users.noreply.github.com>
2022-05-14 20:32:36 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,3 +9,11 @@ function typewriter() {
         setTimeout(typewriter, speed);
     }
 }
+
+function changeImage() {
+    if (document.getElementById("imgClickAndChange").src == "http://www.userinterfaceicons.com/80x80/minimize.png"){
+        document.getElementById("imgClickAndChange").src = "http://www.userinterfaceicons.com/80x80/maximize.png";
+    } else {
+        document.getElementById("imgClickAndChange").src = "http://www.userinterfaceicons.com/80x80/minimize.png";
+    }
+}
