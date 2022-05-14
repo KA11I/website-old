b0fd7d299cc68c30100a044b8e8bb80c165c285c
KA11I <75325289+KA11I@users.noreply.github.com>
2022-05-14 21:02:30 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -30,9 +30,7 @@ setTimeout(function() {
     document.getElementById("imgClickAndChange").src = "https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fwww.uni-kassel.de%2Funi%2Findex.php%3FeID%3DdumpFile%26t%3Dp%26p%3D63987%26token%3D7a38fdf79a187b68173b4466540fe6b936294e61&sp=1652554349Tab0038b84085c7710f3f44eb987349eee4e2395e072980b7e44b5d7a48a50cad";
 }, 3000);
 
-    document.getElementById("imgClickAndChange").src = "https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fimage.geo.de%2F30065630%2Ft%2Fk4%2Fv4%2Fw1440%2Fr0%2F-%2Ffliegenpilz-gross-01-jpg--30419-.jpg&sp=1652554349T911167e3661a32720c8af807b41cc54573df3a6fe07dc4575a37f53fd1f5dfe9";
-
-}
-}
+document.getElementById("imgClickAndChange").src = "https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fimage.geo.de%2F30065630%2Ft%2Fk4%2Fv4%2Fw1440%2Fr0%2F-%2Ffliegenpilz-gross-01-jpg--30419-.jpg&sp=1652554349T911167e3661a32720c8af807b41cc54573df3a6fe07dc4575a37f53fd1f5dfe9";
+    }
 
 
